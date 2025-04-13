82a6e9d84b0eaa2149e9dad16d70c38b8f031b76
3top1a <filip.rusz@email.cz>
2025-04-13 15:42:49 +0200

    Better date parsing

--- data.ts
+++ data.ts
@@ -1,26 +1,73 @@
-import {Plugin, MarkdownView, MarkdownRenderer, TFile, parseYaml} from 'obsidian';
+import {parseYaml, Plugin, TFile} from 'obsidian';
+import dayjs from "dayjs";
 
-export interface ParsedData {
-	start: string,
-	end: string,
+export interface TimeEventData {
+	start: dayjs.Dayjs,
+	end: dayjs.Dayjs,
 }
 
 export interface Data {
 	filename: TFile,
 	content: string,
-	data: ParsedData,
+	data: TimeEventData,
+}
+
+function extractCodeBlocks(markdown: string) {
+	// Matches both ``` and ~~~~ style code blocks
+	const regex = /```time[\s\S]*?```/g;
+	return markdown.match(regex) || [];
 }
 
 export class DataStore {
 	private plugin: Plugin;
 	private data: Data[];
+	private static instance: DataStore;
 
 	constructor(plugin: Plugin) {
 		this.plugin = plugin;
+		DataStore.instance = this;
 	}
+
+	public static getInstance(): DataStore {
+        if (!DataStore.instance) {
+            throw new Error('DataStore not initialized');
+        }
+        return DataStore.instance;
+    }
 
 	async onload() {
 		await this.reload_from_cache();
+	}
+
+	parse_date(input: string): dayjs.Dayjs | null {
+		if (input == "now") {
+			return dayjs();
+		}
+		const date = dayjs(input);
+		if (date.isValid()) {
+			return date;
+		}
+		return null;
+	}
+
+	parse_block_to_data(input: string): TimeEventData | null {
+		const block = input.replace(/```time/g, "").replace(/```/g, "").trim();
+		const parsed_data = parseYaml(block);
+
+		if (parsed_data && typeof parsed_data === 'object' && 'start' in parsed_data && 'end' in parsed_data) {
+			const start = this.parse_date(parsed_data.start);
+			const end = this.parse_date(parsed_data.end);
+			if (!start || !end) {
+				return null;
+			}
+
+			return {
+				start: start,
+				end: end,
+			};
+		}
+
+		return null;
 	}
 
 	async reload_from_cache() {
@@ -34,8 +81,6 @@ export class DataStore {
 			if (!cache) continue;
 			if (!cache?.sections) continue;
 
-			console.debug(cache);
-
 			for (const section of cache.sections) {
 				if (section.type !== 'code') continue;
 
@@ -43,18 +88,16 @@ export class DataStore {
 
 				const blockContent = content.slice(section.position.start.offset, section.position.end.offset).trim();
 
-				if (!blockContent.startsWith("```time")) continue;
-				if (!blockContent.endsWith("```")) continue;
-\`\`\`text([\n.]*)```
-				const block = content.replace("```time", "").replace("```", "").trim();
-				console.debug(section, block)
-				const parsed_data: ParsedData = parseYaml(block);
-
-				timeBlocks.push({
-					content: block,
-					filename: file,
-					data: parsed_data,
-				})
+				const matches = extractCodeBlocks(blockContent);
+				for (const match of matches) {
+					const parsed_data = this.parse_block_to_data(match);
+					if (!parsed_data) continue;
+					timeBlocks.push({
+						content: match,
+						filename: file,
+						data: parsed_data,
+					})
+				}
 			}
 		}
 
--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, WorkspaceLeaf} from 'obsidian';
+import {Notice, Plugin, WorkspaceLeaf} from 'obsidian';
 import {TimelineView, VIEW_TYPE_VIS} from './view'
 import {DataStore} from "./data";
+import localizedFormat from "dayjs/plugin/localizedFormat";
+import dayjs from "dayjs";
+
+dayjs.extend(localizedFormat);
 
 export default class TimelinePlugin extends Plugin {
 	private data: DataStore;
@@ -14,18 +18,32 @@ export default class TimelinePlugin extends Plugin {
 		);
 
 		this.registerMarkdownCodeBlockProcessor('time', (source, element, ctx) => {
-			// TODO Parse and display info
+			this.data.reload_from_cache();
 
-			element.createEl('p', {'text': source})
+			const parsed_data = this.data.parse_block_to_data(source);
+			if (parsed_data) {
+				const {start, end} = parsed_data;
+				element.createEl('p', {'text': `Start: ${start.format('LLL')}, End: ${end.format('LLL')}`});
+			} else {
+				new Notice(`Failed to parse time event data at ${ctx.sourcePath}`);
+				element.createEl('p', {'text': 'Failed to parse time event data.'});
+			}
 		});
 
 		this.addRibbonIcon('chart-no-axes-gantt', 'Timeline', () => {
 			this.activate_view();
 		});
 
+		this.addCommand({
+			name: 'Open Timeline', icon: "chart-no-axes-gantt", id: "opentimeline", callback: () => {
+				this.activate_view();
+			}
+		});
 	}
 
 	async activate_view() {
+		await this.data.reload_from_cache();
+
 		const {workspace} = this.app;
 
 		// If view is already open, bring it to focus
