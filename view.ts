f003389c6d592b7c7ec4f575dd4f3535f2ad4e20
3top1a <filip.rusz@email.cz>
2025-04-13 16:58:25 +0200

    Basic vis-timeline, still example data

--- view.ts
+++ view.ts
@@ -1,8 +1,15 @@
 import {IconName, ItemView, WorkspaceLeaf} from 'obsidian';
+import {Timeline} from 'vis-timeline/esnext'
+import {DataSet} from "vis-data";
+import 'vis-timeline/styles/vis-timeline-graph2d.css';
+import './graph.css';
 
 export const VIEW_TYPE_VIS = 'vistimeline';
 
 export class TimelineView extends ItemView {
+	private timeline: Timeline;
+
+
 	constructor(leaf: WorkspaceLeaf) {
 		super(leaf);
 	}
@@ -22,7 +29,25 @@ export class TimelineView extends ItemView {
 	async onOpen() {
 		const container = this.containerEl.children[1];
 		container.empty();
-		container.createEl('h4', {text: 'Example view'});
+		const timeline_div = container.createDiv('timeline');
+
+		// Create a DataSet (allows two-way data-binding)
+		const items = new DataSet([
+			{id: 1, content: 'item 1', start: '2014-04-20'},
+			{id: 2, content: 'item 2', start: '2014-04-14'},
+			{id: 3, content: 'item 3', start: '2014-04-18'},
+			{id: 4, content: 'item 4', start: '2014-04-16', end: '2014-04-19'},
+			{id: 5, content: 'item 5', start: '2014-04-25'},
+			{id: 6, content: 'item 6', start: '2014-04-27', type: 'point'}
+		]);
+
+		// Configuration for the Timeline
+		const options = {
+			width: '100%',
+		};
+
+		// Create a Timeline
+		this.timeline = new Timeline(timeline_div, items, options);
 	}
 
 	async onClose() {
